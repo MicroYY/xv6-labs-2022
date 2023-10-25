98ba16f0b69058ffcafda80ec65646dc9642841d
MicroYY <jianhuyy@hotmail.com>
2023-10-25 22:14:52 +0800

    thread lab done

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -17,6 +17,7 @@ struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
+pthread_mutex_t lock[NBUCKET];
 
 double
 now()
@@ -47,6 +48,8 @@ void put(int key, int value)
     if (e->key == key)
       break;
   }
+  
+  pthread_mutex_lock(&lock[i]);
   if(e){
     // update the existing key.
     e->value = value;
@@ -54,6 +57,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+  pthread_mutex_unlock(&lock[i]);
 
 }
 
@@ -118,6 +122,7 @@ main(int argc, char *argv[])
     keys[i] = random();
   }
 
+  pthread_mutex_init(&lock, NULL);
   //
   // first the puts
   //
